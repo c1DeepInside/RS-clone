979e731e51637cc5c10854f552a31985d0f205bf
c1DeepInside <vanya0759@gmail.com>
2023-01-31 18:46:29 +0300

    feat: add design for choosing fractions

---
+++ src/components/deckBuilderView/fractionChoose.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="fraction__moves">
+    <div class="fraction__choose">
+      <div class="prev">{{ prevFraction }}</div>
+
+      <div class="arrow_btn">ᐊ</div>
+      <div class="fraction__current">
+        <img class="fraction__emblem" :src="fractionEmblem" alt="Герб фракции" />
+        <div class="fraction__current__name">
+          <div class="current">{{ currFraction }}</div>
+          <div class="fraction__indicator">
+            <span style="color: #a17f40">◆</span>
+            <span>◆</span>
+            <span>◆</span>
+            <span>◆</span>
+          </div>
+        </div>
+      </div>
+      <div class="arrow_btn">ᐅ</div>
+      <div class="next">{{ nextFraction }}</div>
+    </div>
+    <div class="fraction__description">
+      {{ fractionDescription }}
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  data() {
+    return {
+      prevFraction: 'Чудовища',
+      nextFraction: 'Королевства Севера',
+      currFraction: 'Королевства Севера',
+      fractionEmblem: 'src/assets/images/faction-emblems/monsters_emblem.png',
+      fractionDescription: 'Королевства Севера берут карту из своей колоды после каждого выигранного раунда',
+      fractions: ['Королевства Севера', 'Нильфгаард', "Скоя'таэлиэ", 'Чудовища'] as string[],
+    };
+  },
+});
+</script>
+<style lang="scss" scoped>
+.fraction__moves {
+  width: 70%;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  color: white;
+}
+
+.fraction__indicator {
+  color: #212321;
+  font-size: 1.1vw;
+}
+
+.fraction__choose {
+  gap: 2%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.fraction__current {
+  display: flex;
+  justify-content: center;
+  width: 50%;
+}
+.fraction__current__name {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-self: center;
+  align-self: end;
+}
+.fraction__emblem {
+  width: 13%;
+  object-fit: contain;
+}
+.fraction__description {
+  height: 30%;
+  width: 100%;
+  font-size: 1.35vw;
+  color: #867762;
+  text-align: center;
+}
+
+.arrow_btn {
+  font-size: 2.1vw;
+  color: #685942;
+
+  &:hover {
+    color: #937c59;
+  }
+}
+
+.current {
+  font-size: 1.6vw;
+}
+
+.prev {
+  text-align: end;
+}
+
+.next,
+.prev {
+  width: 20%;
+  color: #685942;
+  font-size: 1.1vw;
+}
+</style>
--- src/views/DeckBuilderView.vue
+++ src/views/DeckBuilderView.vue
@@ -1,33 +1,115 @@
-<script setup lang="ts"></script>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import fractionChoose from '@/components/deckBuilderView/fractionChoose.vue';
+
+export default defineComponent({
+  data() {
+    return {
+      
+    }
+  },
+  components: {
+    fractionChoose,
+  }
+});
+</script>
 
 <template>
   <main class="main">
     <video autoplay muted loop class="background">
       <source src="@/assets/videos/backFog.mp4" type="video/mp4" />
     </video>
+    <div class="UI">
+      <div class="fraction">
+        <div class="collection__cart">
+          <p class="collection__text">Коллекция карт</p>
+        </div>
+        <fractionChoose></fractionChoose>
+        <div class="collection__deck">
+          <p class="collection__text">Карты колоды</p>
+        </div>
+      </div>
+
+      <div class="builder">
+        <div class="collection"></div>
+        <div class="info"></div>
+        <div class="deck"></div>
+      </div>
+    </div>
   </main>
 </template>
 
 <style lang="scss">
-$DECK_COLOR: #4e382d;
+$DECK_COLOR: #4d392c;
 
 html {
   &::-webkit-scrollbar {
     display: none;
   }
+
+  background-color: #171617;
 }
 
 .main {
+  position: relative;
+  z-index: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   background-color: black;
   width: 100vw;
   height: calc(100vw * 9 / 16);
-  overflow: hidden;
 }
 
 .background {
+  z-index: 1;
   width: 100vw;
   height: calc(100vw * 9 / 16);
   position: absolute;
   opacity: 0.3;
+}
+
+.UI {
+  width: 85%;
+  height: 90%;
+  border: white 1px solid;
+  position: relative;
+  z-index: 2;
+  display: flex;
+  flex-direction: column;
+}
+
+.fraction {
+  display: flex;
+  justify-content: space-between;
+  height: 20%;
+
+  &__choose {
+    flex-grow: 1;
+  }
+}
+
+.builder {
+  display: flex;
+  height: 100%;
+
+  .collection {
+    width: 40%;
+    height: 100%;
+  }
+
+  .info {
+    width: 20%;
+    height: 100%;
+  }
+
+  .deck {
+    width: 40%;
+    height: 100%;
+  }
+}
+
+.collection__text {
+  color: white;
 }
 </style>
