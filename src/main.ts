bfbdabb7b8299946d1a55c10e3a6efe0aa4a7f8a
PARAZAKATAKANKA <heavens1xdoor@mail.ru>
2023-01-31 09:04:13 +0300

    refactor: change url styles

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
-import "./styles/reset.scss";
+import "./styles/common.scss";
 
 import App from "./App.vue";
 import router from "./router";
