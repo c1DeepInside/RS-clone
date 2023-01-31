305af86e934a4c799c3ae0166d3245ef951928d1
c1DeepInside <vanya0759@gmail.com>
2023-01-31 15:52:39 +0300

    feat: add background

--- src/views/DeckBuilderView.vue
+++ src/views/DeckBuilderView.vue
@@ -1,7 +1,33 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main>
-    <h1>deck</h1>
+  <main class="main">
+    <video autoplay muted loop class="background">
+      <source src="@/assets/videos/backFog.mp4" type="video/mp4" />
+    </video>
   </main>
 </template>
+
+<style lang="scss">
+$DECK_COLOR: #4e382d;
+
+html {
+  &::-webkit-scrollbar {
+    display: none;
+  }
+}
+
+.main {
+  background-color: black;
+  width: 100vw;
+  height: calc(100vw * 9 / 16);
+  overflow: hidden;
+}
+
+.background {
+  width: 100vw;
+  height: calc(100vw * 9 / 16);
+  position: absolute;
+  opacity: 0.3;
+}
+</style>
