6fdeb43320b4bc013c40cb5e3bbd4fe24d487f40
Sobolevskiy Vitaliy <SobVitalya@yandex.ru>
2023-01-31 13:46:29 +0700

    fead: create GameView

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import IntroView from "../views/IntroView.vue";
+import GameView from "../views/GameView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: IntroView,
+    },
+    {
+      path: "/game",
+      name: "game",
+      component: GameView,
     },
   ],
 });
