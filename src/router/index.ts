b1b444b3e96dfebcb683098431073604613991b8
c1DeepInside <vanya0759@gmail.com>
2023-01-31 13:03:04 +0300

    feat: add deck builder view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import IntroView from '../views/IntroView.vue';
+import DeckBuilderView from '../views/DeckBuilderView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: IntroView,
+    },
+    {
+      path: '/deck',
+      name: 'deck',
+      component: DeckBuilderView,
     },
   ],
 });
---
+++ src/views/DeckBuilderView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1>deck</h1>
+  </main>
+</template>
