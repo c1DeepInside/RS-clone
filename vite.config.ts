7f2bbc205c7777cd8b886391360767a4272cdbd4
PARAZAKATAKANKA <heavens1xdoor@mail.ru>
2023-01-31 11:58:30 +0300

    feat: configure global variables

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,11 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/variables.scss";`
+      }
+    }
+  }
 });
