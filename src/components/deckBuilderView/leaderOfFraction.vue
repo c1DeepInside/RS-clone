73fe273090b11d4d55a8ac2bc61bbeafaa36b291
c1DeepInside <vanya0759@gmail.com>
2023-02-01 16:14:03 +0300

    feat: add leader selection modal

--- src/components/deckBuilderView/leaderOfFraction.vue
+++ src/components/deckBuilderView/leaderOfFraction.vue
@@ -3,18 +3,36 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   data() {
-    return {};
+    return {
+      ifModal: false,
+    };
+  },
+  methods: {
+    showModal() {
+      this.ifModal = !this.ifModal;
+    },
   },
 });
 </script>
 
 <template>
-  <div class="leader__img__wrap">
+  <div class="leader__img__wrap" @click="showModal">
     <img class="leader__img" src="src/assets/images/leader.png" alt="лидер" />
   </div>
+  <div v-if="ifModal" class="modal" @click="showModal"></div>
 </template>
 
 <style lang="scss" scoped>
+.modal {
+  position: fixed;
+  z-index: 2;
+  background-color: rgba($color: #000000, $alpha: 0.9);
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  
+}
 .leader__img__wrap {
   box-sizing: border-box;
   width: 50%;
