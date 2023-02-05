f9beca689b7759b26bffe31b98f9970523a5b44b
PARAZAKATAKANKA <heavens1xdoor@mail.ru>
2023-02-05 21:40:15 +0300

    feat: add card interface

---
+++ src/interfaces/card.ts
@@ -0,0 +1,18 @@
+type CardAbility = 'agile' | 'bond' | 'decoy' | 'medic' | 'spy' | 'morale' | 'horn' | 'muster' | 'scorch' | 'rain';
+
+type CardFieldType = 'melee' | 'range' | 'siege' | 'boost' | 'weather';
+
+type CardType = 'hero' | 'usual' | 'special' | 'leader';
+
+export default interface Card {
+  id: number;
+  name: string;
+  type: CardType;
+  image: string;
+  description: string;
+  fractionId: number | null;
+  ability: CardAbility | null;
+  fieldType: CardFieldType[];
+  power: number | null;
+  quantity: number;
+}
