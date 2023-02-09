461e166027f0f42f053e3efbdfe4ebe9d41ffbd8
PARAZAKATAKANKA <heavens1xdoor@mail.ru>
2023-02-09 17:11:34 +0300

    feat: add background deck

--- src/utilits/cardBuildImgs.ts
+++ src/utilits/cardBuildImgs.ts
@@ -18,10 +18,17 @@ export const cardAbilitiesImg: Record<CardAbility, string> = {
 };
 
 export const cardFractionsImg: Record<number, string> = {
-  3: '/src/assets/images/build/card_faction_banner_nifgaard.png',
-  2: '/src/assets/images/build/card_faction_banner_no_mans_land.png',
+  1: '/src/assets/images/build/card_faction_banner_nilfgaard.png',
+  3: '/src/assets/images/build/card_faction_banner_no_mans_land.png',
   0: '/src/assets/images/build/card_faction_banner_northern_realms.png',
-  1: '/src/assets/images/build/card_faction_banner_scoiatael.png',
+  2: '/src/assets/images/build/card_faction_banner_scoiatael.png',
+};
+
+export const fractionsDeckImg: Record<number, string> = {
+  1: '/src/assets/images/back-deck/deck_back_nilfgaard.jpg',
+  3: '/src/assets/images/back-deck/deck_back_monsters.jpg',
+  0: '/src/assets/images/back-deck/deck_back_realms.jpg',
+  2: '/src/assets/images/back-deck/deck_back_scoiatael.jpg',
 };
 
 export type CardEquipmendType = 'melee' | 'range' | 'siege' | 'melee_range';
