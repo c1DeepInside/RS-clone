93e27e496394c0e2902362904f63129410602bdb
Sobolevskiy Vitaliy <SobVitalya@yandex.ru>
2023-02-15 23:15:10 +0700

    feat: add login http

--- src/api/userAPI.ts
+++ src/api/userAPI.ts
@@ -1,15 +1,33 @@
 const baseURL = '';
 
-export const registration = async (login: string, pass: string) => {
+export const registration = async (username: string, password: string) => {
   const response: Response = await fetch(`${baseURL}/users/register/`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ username: login, password: pass }),
+    body: JSON.stringify({ username, password }),
   });
 
   if (!response.ok) {
     throw new Error('Registration failed');
   }
+};
+
+export const login = async (username: string, password: string) => {
+  const response: Response = await fetch(`${baseURL}/users/login/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ username, password }),
+  });
+
+  if (!response.ok) {
+    throw new Error('Login failed');
+  }
+
+  const data: { token: string } = await response.json();
+  localStorage.setItem('token', data.token);
+  return data;
 };
